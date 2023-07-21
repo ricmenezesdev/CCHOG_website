a6eb273f6364400b09bda2f4e36657f123abc266
ricmenezesdev <ricmenezesdev@gmail.com>
2023-07-21 15:45:37 -0400

    hoverme

--- HOGwebpg/funtions.js
+++ HOGwebpg/funtions.js
@@ -2,13 +2,16 @@
     function menuButton() {
         document.getElementById("myDropdown").classList.toggle("show");
     }
-    window.addEventListener("mouseover");
+    
     window.addEventListener("click", function(event) {
-        if (!event.target.matches(".dropbtn")) {
+            
+        if (!event.target.matches(".dropbtn") && !event.target.matches(".fa-bars")) {
             var dropdowns = document.getElementsByClassName("dropdown-content");
             for (var i = 0; i < dropdowns.length; i++) {
                 var openDropdown = dropdowns[i];
-                
+                if (openDropdown.classList.contains("show")) {
+                   openDropdown.classList.remove("show");
+                }
             }
         }
     });
